2c8303e6b271a200819622fa3fd8aac466aa6006
Drosscend <47970260+Drosscend@users.noreply.github.com>
2023-12-12 22:07:20 +0100

    [Homepage] : Create first homepage template

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -1,3 +1,61 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main>Hello World</main>;
+  const links = [
+    { name: "About", href: "#" },
+    { name: "Projects", href: "#" },
+    { name: "Contact", href: "#" },
+    { name: "Login", href: "#" },
+  ];
+
+  return (
+    <div className="flex min-h-screen flex-col bg-gray-100 dark:bg-gray-900">
+      <header className="flex h-14 items-center px-4 lg:px-6">
+        <Link className="flex items-center justify-center" href="#">
+          <span>Comtpa Fruits</span>
+        </Link>
+        <nav className="ml-auto flex gap-4 sm:gap-6">
+          {links.map((link) => (
+            <Link
+              key={link.name}
+              className="text-sm font-medium underline-offset-4 hover:underline"
+              href={link.href}
+            >
+              {link.name}
+            </Link>
+          ))}
+        </nav>
+      </header>
+      <main className="flex-1">
+        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-72">
+          <div className="container px-4 md:px-6">
+            <div className="flex flex-col items-center space-y-4 text-center">
+              <div className="space-y-2">
+                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+                  Welcome to Compta Fruits
+                </h1>
+                <p className="mx-auto max-w-[700px] text-gray-500 dark:text-gray-400 md:text-xl">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus nec iaculis mauris.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t px-4 py-6 sm:flex-row md:px-6">
+        <p className="text-xs text-gray-500 dark:text-gray-400">
+          © Compta Fruits. All rights reserved.
+        </p>
+        <nav className="flex gap-4 sm:ml-auto sm:gap-6">
+          <Link className="text-xs underline-offset-4 hover:underline" href="#">
+            Terms of Service
+          </Link>
+          <Link className="text-xs underline-offset-4 hover:underline" href="#">
+            Privacy Policy
+          </Link>
+        </nav>
+      </footer>
+    </div>
+  );
 }
