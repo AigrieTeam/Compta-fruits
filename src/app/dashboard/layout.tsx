01e32492c868b94490879e41f21616c363afa3de
Drosscend <47970260+Drosscend@users.noreply.github.com>
2023-12-12 21:48:59 +0100

    [Dashboard] : Setup base template design

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "../globals.css";
+
+import TeamSwitcher from "@/components/team-switcher";
+import { Search } from "@/components/search";
+import { UserNav } from "@/components/user-nav";
+import { MainNav } from "@/components/main-nav";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Compta Fruits",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="flex flex-col">
+          <div className="border-b">
+            <div className="flex h-16 items-center px-4">
+              <TeamSwitcher />
+              <MainNav className="mx-6" />
+              <div className="ml-auto flex items-center space-x-4">
+                <Search />
+                <UserNav />
+              </div>
+            </div>
+          </div>
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
