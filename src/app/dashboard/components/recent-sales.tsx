568e1db1e942f267e79888f9d11c554f6c01b09e
Drosscend <47970260+Drosscend@users.noreply.github.com>
2023-12-12 22:06:59 +0100

    [Dashboard] : Reformat code

--- src/app/dashboard/components/recent-sales.tsx
+++ src/app/dashboard/components/recent-sales.tsx
@@ -1,67 +1,56 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function RecentSales() {
+  const details = [
+    {
+      name: "Olivia Martin",
+      email: "olivia.martin@email.com",
+      avatar: "/avatars/01.png",
+      amount: "+$1,999.00",
+    },
+    {
+      name: "Jackson Lee",
+      email: "jackson.lee@email.com",
+      avatar: "/avatars/02.png",
+      amount: "+$39.00",
+    },
+    {
+      name: "Isabella Nguyen",
+      email: "isabella.nguyen@email.com",
+      avatar: "/avatars/03.png",
+      amount: "+$299.00",
+    },
+    {
+      name: "William Kim",
+      email: "william.kim@email",
+      avatar: "/avatars/04.png",
+      amount: "+$99.00",
+    },
+    {
+      name: "Sofia Davis",
+      email: "sofia.davis@email.com",
+      avatar: "/avatars/05.png",
+      amount: "+$39.00",
+    },
+  ];
+
   return (
     <div className="space-y-8">
-      <div className="flex items-center">
-        <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/01.png" alt="Avatar" />
-          <AvatarFallback>OM</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Olivia Martin</p>
-          <p className="text-muted-foreground text-sm">
-            olivia.martin@email.com
-          </p>
+      {details.map((detail) => (
+        <div key={detail.email} className="flex items-center">
+          <Avatar className="h-9 w-9">
+            <AvatarImage src={detail.avatar} alt="Avatar" />
+            <AvatarFallback>
+              {detail.name.split(" ").map((n) => n[0])}
+            </AvatarFallback>
+          </Avatar>
+          <div className="ml-4 space-y-1">
+            <p className="text-sm font-medium leading-none">{detail.name}</p>
+            <p className="text-muted-foreground text-sm">{detail.email}</p>
+          </div>
+          <div className="ml-auto font-medium">{detail.amount}</div>
         </div>
-        <div className="ml-auto font-medium">+$1,999.00</div>
-      </div>
-      <div className="flex items-center">
-        <Avatar className="flex h-9 w-9 items-center justify-center space-y-0 border">
-          <AvatarImage src="/avatars/02.png" alt="Avatar" />
-          <AvatarFallback>JL</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Jackson Lee</p>
-          <p className="text-muted-foreground text-sm">jackson.lee@email.com</p>
-        </div>
-        <div className="ml-auto font-medium">+$39.00</div>
-      </div>
-      <div className="flex items-center">
-        <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/03.png" alt="Avatar" />
-          <AvatarFallback>IN</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Isabella Nguyen</p>
-          <p className="text-muted-foreground text-sm">
-            isabella.nguyen@email.com
-          </p>
-        </div>
-        <div className="ml-auto font-medium">+$299.00</div>
-      </div>
-      <div className="flex items-center">
-        <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/04.png" alt="Avatar" />
-          <AvatarFallback>WK</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">William Kim</p>
-          <p className="text-muted-foreground text-sm">will@email.com</p>
-        </div>
-        <div className="ml-auto font-medium">+$99.00</div>
-      </div>
-      <div className="flex items-center">
-        <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/05.png" alt="Avatar" />
-          <AvatarFallback>SD</AvatarFallback>
-        </Avatar>
-        <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Sofia Davis</p>
-          <p className="text-muted-foreground text-sm">sofia.davis@email.com</p>
-        </div>
-        <div className="ml-auto font-medium">+$39.00</div>
-      </div>
+      ))}
     </div>
   );
 }
